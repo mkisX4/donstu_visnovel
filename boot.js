ed0d6e484fbb4332012221c231ac4574da9f6ba4
Lorenhow <105514629+Lorenhow@users.noreply.github.com>
2024-06-29 17:54:24 +0300

    0.1
    Placeholders are in place!

--- boot.js
+++ boot.js
@@ -1,7 +1,7 @@
 const RenJSConfig =  {
-  'name': 'S.T.A.R.',
-  'w': 800,
-  'h': 600,
+  'name': 'Live Donstu',
+  'w': 1024,
+  'h': 768,
   'renderer': Phaser.AUTO, // become renderer
   'scaleMode': Phaser.ScaleManager.SHOW_ALL,
   'loadingScreen': {
@@ -9,12 +9,12 @@ const RenJSConfig =  {
     'loadingBar': {
       'asset': 'assets/gui/loaderloading-bar.png',
       'position': {
-        'x': 109,
-        'y': 458
+        'x': 256,
+        'y': 460
       },
       'size': {
-        'w': 578,
-        'h': 82
+        'w': 500,
+        'h': 128
       }
     }
   },
