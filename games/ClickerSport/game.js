57c33796939a06dc86c552a1773be32a23635605
Lorenhow <105514629+Lorenhow@users.noreply.github.com>
2024-07-09 17:01:31 +0300

    0.5
    More or less worknig map. Another game is now playable. Hope it'll end soon.

--- games/ClickerSport/game.js
+++ games/ClickerSport/game.js
@@ -1,106 +1,233 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let score = 0;
-    let strength = 1;
-    let endurance = 1;
-    let strengthLevel = 1;
-    let enduranceLevel = 1;
-    let strengthCost = 15;
-    let enduranceCost = 30;
-    let doubleClickPurchased = false;
-    let autoclickerPurchased = false;
-    let autoclickerInterval = null;
+class ClickerSport_game extends RenJS.Plugin {
+    onCall(){    
+        const TheGame = this.game
+        TheGame.canvas.style.display = "none";
+        const gameBody = TheGame.canvas.parentElement.getElementsByTagName("div")[3];
+        gameBody.style.display = "block";
 
-    const weightFirst = document.querySelector('.weight-first');
-    const weightSecond = document.querySelector('.weight-second');
-    const button = document.getElementById('clicker');
-    const scoreDisplay = document.getElementById('score');
-    const strengthLevelDisplay = document.getElementById('strength-level');
-    const enduranceLevelDisplay = document.getElementById('endurance-level');
-    const strengthCostDisplay = document.getElementById('strength-cost');
-    const enduranceCostDisplay = document.getElementById('endurance-cost');
-    const upgradeStrengthButton = document.getElementById('upgrade-strength');
-    const upgradeEnduranceButton = document.getElementById('upgrade-endurance');
-    const doubleClickButton = document.getElementById('double-click');
-    const autoclickerButton = document.getElementById('autoclicker');
-    const titleDisplay = document.getElementById('titul');
+        gameBody.parentElement.setAttribute('style', 'background-image: url(http://127.0.0.1:5500/games/ClickerSport/images/sportzal.jpg); background-repeat: no-repeat; background-position: center');
 
-    const titles = ['Новичок', 'Специалист', 'Мастер', 'Эксперт', 'Легенда'];
+        let header = document.createElement('header');
+        header.classList.add('header-title');
+        header.innerHTML = 'Магазин';
+        let store_button1 = document.createElement('button');
+        store_button1.classList.add('header-item');
+        store_button1.id = 'double-click';
+        store_button1.innerHTML = 'Двойной клик <br />300 points';
+        header.appendChild(store_button1);
+        let store_button2 = document.createElement('button');
+        store_button2.classList.add('header-item');
+        store_button2.id = 'autoclicker';
+        store_button2.innerHTML = 'Автокликер <br />500 points';
+        header.appendChild(store_button2);
 
-    button.addEventListener('click', () => {
-        let pointsToAdd = 3 * strength;
-        if (doubleClickPurchased) {
-            pointsToAdd *= 2;
-        }
-        score += pointsToAdd;
-        scoreDisplay.textContent = `Очки: ${score}`;
+        gameBody.appendChild(header);
 
-        button.disabled = true;
+        let main_div = document.createElement('div');
+        main_div.classList.add('main');
 
-        weightFirst.style.top = '60px';
-        weightSecond.style.top = '60px';
+        let skill_menu_div = document.createElement('div');
+        skill_menu_div.classList.add('skills-menu');
+        let skill_span = document.createElement('span');
+        skill_span.classList.add('title_for_blocks');
+        skill_span.innerHTML = 'Навыки';
+        skill_menu_div.appendChild(skill_span);
+        let skill_list_div = document.createElement('div');
+        skill_list_div.classList.add('skills-list');
+        let strength_span = document.createElement('span');
+        strength_span.classList.add('skill-item');
+        let strength_button = document.createElement('button');
+        strength_button.classList.add('skill-btn');
+        strength_button.id = 'upgrade-strength';
+        strength_button.innerHTML = 'Улучшить силу -';
+        let str_cost_span = document.createElement('span');
+        str_cost_span.id = 'strength-cost';
+        str_cost_span.innerHTML = '15';
+        strength_button.appendChild(str_cost_span);
+        strength_span.appendChild(strength_button);
+        skill_list_div.appendChild(strength_span);
+        let endurance_span = document.createElement('span');
+        endurance_span.classList.add('skill-item');
+        let endurance_button = document.createElement('button');
+        endurance_button.classList.add('skill-btn');
+        endurance_button.id = 'upgrade-endurance';
+        endurance_button.innerHTML = 'Улучшить выносливость -';
+        let end_cost_span = document.createElement('span');
+        end_cost_span.id = 'endurance-cost';
+        end_cost_span.innerHTML = '30';
+        endurance_button.appendChild(end_cost_span);
+        endurance_span.appendChild(endurance_button);
+        skill_list_div.appendChild(endurance_span);
+        skill_menu_div.appendChild(skill_list_div);
+        main_div.appendChild(skill_menu_div);
 
-        setTimeout(() => {
-            weightFirst.style.top = '180px';
-            weightSecond.style.top = '180px';
+        let container_div = document.createElement('div');
+        container_div.classList.add('container');
+        let title_div = document.createElement('div');
+        title_div.classList.add('titul');
+        let title_span = document.createElement('span');
+        title_span.innerHTML='Титул:';
+        title_div.appendChild(title_span);
+        let actual_title_span = document.createElement('span');
+        actual_title_span.id = 'titul';
+        actual_title_span.innerHTML = 'Новичок';
+        title_div.appendChild(actual_title_span);
+        container_div.appendChild(title_div);
+        let lifter_div = document.createElement('div');
+        lifter_div.id = 'weightlifter';
+        let first_weight = document.createElement('div');
+        first_weight.classList.add('weight', 'weight-first');
+        lifter_div.appendChild(first_weight);
+        let second_weight = document.createElement('div');
+        second_weight.classList.add('weight', 'weight-second');
+        lifter_div.appendChild(second_weight);
+        container_div.appendChild(lifter_div);
+        let clicker_button = document.createElement('button');
+        clicker_button.classList.add('clicker_btn');
+        clicker_button.id = 'clicker';
+        clicker_button.innerHTML = 'Жми!';
+        container_div.appendChild(clicker_button);
+        let score_div = document.createElement('div');
+        score_div.classList.add('score_table');
+        score_div.id = 'score';
+        score_div.innerHTML = 'Очки: 0';
+        container_div.appendChild(score_div);
+        main_div.appendChild(container_div);
+
+        let statistics_div = document.createElement('div');
+        statistics_div.classList.add('statistics');
+        let stat_title_span = document.createElement('span');
+        stat_title_span.classList.add('title_for_blocks');
+        stat_title_span.innerHTML = 'Статистика';
+        statistics_div.appendChild(stat_title_span);
+        let statistics_list_div = document.createElement('div');
+        statistics_list_div.classList.add('statistics_list');
+        let strength_stat_span = document.createElement('span');
+        strength_stat_span.classList.add('statistics-item');
+        strength_stat_span.innerHTML = 'Сила lvl:';
+        let str_stat_inner_span = document.createElement('span');
+        str_stat_inner_span.id = 'strength-level';
+        str_stat_inner_span.innerHTML = '1';
+        strength_stat_span.appendChild(str_stat_inner_span);
+        statistics_list_div.appendChild(strength_stat_span);
+        let endurance_stat_span = document.createElement('span');
+        endurance_stat_span.classList.add('statistics-item');
+        endurance_stat_span.innerHTML = 'Выносливость lvl:';
+        let end_stat_inner_span = document.createElement('span');
+        end_stat_inner_span.id = 'endurance-level';
+        end_stat_inner_span.innerHTML = '1';
+        endurance_stat_span.appendChild(end_stat_inner_span);
+        statistics_list_div.appendChild(endurance_stat_span);
+        statistics_div.appendChild(statistics_list_div);
+
+        main_div.appendChild(statistics_div);
+        gameBody.appendChild(main_div)
+
+        let score = 0;
+        let strength = 1;
+        let endurance = 1;
+        let strengthLevel = 1;
+        let enduranceLevel = 1;
+        let strengthCost = 15;
+        let enduranceCost = 30;
+        let doubleClickPurchased = false;
+        let autoclickerPurchased = false;
+        let autoclickerInterval = null;
+
+        const weightFirst = gameBody.querySelector('.weight-first');
+        const weightSecond = gameBody.querySelector('.weight-second');
+        const button = document.getElementById('clicker');
+        const scoreDisplay = document.getElementById('score');
+        const strengthLevelDisplay = document.getElementById('strength-level');
+        const enduranceLevelDisplay = document.getElementById('endurance-level');
+        const strengthCostDisplay = document.getElementById('strength-cost');
+        const enduranceCostDisplay = document.getElementById('endurance-cost');
+        const upgradeStrengthButton = document.getElementById('upgrade-strength');
+        const upgradeEnduranceButton = document.getElementById('upgrade-endurance');
+        const doubleClickButton = document.getElementById('double-click');
+        const autoclickerButton = document.getElementById('autoclicker');
+        const titleDisplay = document.getElementById('titul');
+
+        const titles = ['Новичок', 'Специалист', 'Мастер', 'Эксперт', 'Легенда'];
+
+        button.addEventListener('click', () => {
+            let pointsToAdd = 3 * strength;
+            if (doubleClickPurchased) {
+                pointsToAdd *= 2;
+            }
+            score += pointsToAdd;
+            scoreDisplay.textContent = `Очки: ${score}`;
+
+            button.disabled = true;
+
+            weightFirst.style.top = '60px';
+            weightSecond.style.top = '60px';
 
             setTimeout(() => {
-                button.disabled = false;
-            }, 1000 / endurance);
-        }, 900 / endurance);
-    });
+                weightFirst.style.top = '180px';
+                weightSecond.style.top = '180px';
 
-    upgradeStrengthButton.addEventListener('click', () => {
-        if (score >= strengthCost) {
-            score -= strengthCost;
-            strength++;
-            strengthLevel++;
-            strengthCost = Math.ceil(strengthCost * 4);
-            updateDisplay();
-        }
-    });
+                setTimeout(() => {
+                    button.disabled = false;
+                }, 1000 / endurance);
+            }, 900 / endurance);
+        });
 
-    upgradeEnduranceButton.addEventListener('click', () => {
-        if (score >= enduranceCost) {
-            score -= enduranceCost;
-            endurance++;
-            enduranceLevel++;
-            enduranceCost = Math.ceil(enduranceCost * 4);
-            updateDisplay();
-        }
-    });
+        upgradeStrengthButton.addEventListener('click', () => {
+            if (score >= strengthCost) {
+                score -= strengthCost;
+                strength++;
+                strengthLevel++;
+                strengthCost = Math.ceil(strengthCost * 4);
+                updateDisplay();
+            }
+        });
 
-    doubleClickButton.addEventListener('click', () => {
-        if (score >= 300 && !doubleClickPurchased) {
-            score -= 300;
-            doubleClickPurchased = true;
-            doubleClickButton.disabled = true;
-            updateDisplay();
-        }
-    });
+        upgradeEnduranceButton.addEventListener('click', () => {
+            if (score >= enduranceCost) {
+                score -= enduranceCost;
+                endurance++;
+                enduranceLevel++;
+                enduranceCost = Math.ceil(enduranceCost * 4);
+                updateDisplay();
+            }
+        });
 
-    autoclickerButton.addEventListener('click', () => {
-        if (score >= 500 && !autoclickerPurchased) {
-            score -= 500;
-            autoclickerPurchased = true;
-            autoclickerButton.disabled = true;
-            autoclickerInterval = setInterval(() => {
-                button.click();
-            }, 1000); // 
-            updateDisplay();
+        doubleClickButton.addEventListener('click', () => {
+            if (score >= 300 && !doubleClickPurchased) {
+                score -= 300;
+                doubleClickPurchased = true;
+                doubleClickButton.disabled = true;
+                updateDisplay();
+            }
+        });
+
+        autoclickerButton.addEventListener('click', () => {
+            if (score >= 500 && !autoclickerPurchased) {
+                score -= 500;
+                autoclickerPurchased = true;
+                autoclickerButton.disabled = true;
+                autoclickerInterval = setInterval(() => {
+                    button.click();
+                }, 1000); // 
+                updateDisplay();
+            }
+        });
+        function updateTitle() {
+            const titleIndex = Math.floor(strengthLevel / 10);
+            const title = titles[Math.min(titleIndex, titles.length - 1)];
+            titleDisplay.textContent = title;
         }
-    });
-    function updateTitle() {
-        const titleIndex = Math.floor(strengthLevel / 10);
-        const title = titles[Math.min(titleIndex, titles.length - 1)];
-        titleDisplay.textContent = title;
+
+        function updateDisplay() {
+            scoreDisplay.textContent = `Очки: ${score}`;
+            strengthLevelDisplay.textContent = strengthLevel;
+            enduranceLevelDisplay.textContent = enduranceLevel;
+            strengthCostDisplay.textContent = strengthCost;
+            enduranceCostDisplay.textContent = enduranceCost;
+            updateTitle();
+        }
     }
+}
 
-    function updateDisplay() {
-        scoreDisplay.textContent = `Очки: ${score}`;
-        strengthLevelDisplay.textContent = strengthLevel;
-        enduranceLevelDisplay.textContent = enduranceLevel;
-        strengthCostDisplay.textContent = strengthCost;
-        enduranceCostDisplay.textContent = enduranceCost;
-        updateTitle();
-    }
-});
+RenJSGame.addPlugin('ClickerSport_game',ClickerSport_game)
--- map/map.js
+++ map/map.js
@@ -1,139 +1,114 @@
-class PointAndClick extends RenJS.Plugin {
+class GameMap extends RenJS.Plugin {
   config = null;
   infoPaC = null;
 
-  onStart() {
-    this.config = this.game.setup.pointAndClick;
-    this.infoPaC = { removed: {}, added: {} };
-    this.game.managers.logic.vars.pointAndClick = this.infoPaC;
-  }
+  onCall() {
+    const TheGame = this.game;
+    TheGame.canvas.style.display = "none";
+    const mapBody = TheGame.canvas.parentElement.getElementsByTagName("div")[5];
+    mapBody.style.display = "block";
 
-  onLoad(slot, data) {
-    this.infoPaC = data.vars.pointAndClick;
-  }
+    mapBody.parentElement.setAttribute('style', 'background-image: url(http://127.0.0.1:5500/map/images/map.jpg); background-repeat: no-repeat; background-position: center');
 
-  onInit() {
-    this.game.screenEffects.transition['PaCTransition'] = (from, to, position, scaleX) => {
-      let room = to.name;
-      if (this.config[room]) {
-        for (const obj in this.config[room]) {
-          if (this.infoPaC.removed[room] && this.infoPaC.removed[room][obj]) {
-            continue;
-          }
-          const attr = this.config[room][obj].split(" ");
-          let newObj = {
-            name: obj,
-            key: attr[0],
-            x: parseInt(attr[1]) - to.width / 2,
-            y: parseInt(attr[2]) - to.height / 2,
-            scene: attr[3]
-          }
-          this.createObject(to, newObj);
-        }
-      }
-      if (this.infoPaC.added[room]) {
-        for (const obj in this.infoPaC.added[room]) {
-          this.createObject(to, this.infoPaC.added[room][obj]);
-        }
-      }
-      this.game.input.enabled = true;
-      return this.game.screenEffects.transition.FADETOBLACK(from, to, position, scaleX);
-    }
-  }
-
-  createObject(room, obj, transition) {
-    if (!room.pointAndClick) {
-      room.pointAndClick = {
-        map: {},
-        group: this.game.add.group()
-      }
-      room.addChild(room.pointAndClick.group);
+    if (TheGame.managers.logic.vars['day'] == 5){
+      TheGame.managers.story.startScene('exam_'+TheGame.managers.logic.vars['mc_type']);
+      TheGame.resolveAction();
+      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
+      mapBody.innerHTML='';
+      mapBody.style.display = "none";
+      TheGame.canvas.style.display = "block";
     }
 
-    let key = obj.key;
-    if (key === 'home' || key === 'park' || key === 'blockHome' || key === 'blockPark') {
-      const btn = this.game.add.button(obj.x, obj.y, key, async () => {
-        if (!this.game.managers.story.interpreting) {
-          room.pointAndClick.group.ignoreChildInput = true;
-          if (key === 'home') {
-            await this.game.managers.story.startScene('homeScene');
-          } else if (key === 'park') {
-            await this.game.managers.story.startScene('parkScene');
-          }
-          this.game.resolveAction();
-        }
-      }, this.game, 1, 0, 1, 0, room.pointAndClick.group);
-      room.pointAndClick.map[obj.name] = btn;
-    } else {
-      this.game.add.sprite(obj.x, obj.y, key, null, room.pointAndClick.group);
+    let temprorary_time = parseInt(TheGame.managers.logic.vars['time']);
+    switch(temprorary_time){
+      case 1:
+        let morningButton = document.createElement('button');
+        morningButton.classList.add('walk_button');
+        morningButton.id = 'morning_walk';
+        mapBody.appendChild(morningButton);
+        break;
+      case 2:
+        let dayButtonStudy = document.createElement('button');
+        dayButtonStudy.classList.add('study_button');
+        dayButtonStudy.id = 'day_study';
+        mapBody.appendChild(dayButtonStudy);
+        let dayButtonFriendly = document.createElement('button');
+        dayButtonFriendly.classList.add('frienly_button');
+        dayButtonFriendly.id = 'day_friendly';
+        mapBody.appendChild(dayButtonFriendly);
+        break;
+      case 3:
+        let eveningButtonWalk = document.createElement('button');
+        eveningButtonWalk.classList.add('walk_button');
+        eveningButtonWalk.id = 'evening_walk';
+        mapBody.appendChild(eveningButtonWalk);
+        let eveningButtonStudy = document.createElement('button');
+        eveningButtonStudy.classList.add('study_button');
+        eveningButtonStudy.id = 'evening_study';
+        mapBody.appendChild(eveningButtonStudy);
+        let eveningButtonFriendly = document.createElement('button');
+        eveningButtonFriendly.classList.add('frienly_button');
+        eveningButtonFriendly.id = 'evening_friendly';
+        mapBody.appendChild(eveningButtonFriendly);
+        break;
+      default:
+        console.log(TheGame.managers.logic.vars['time']);
+        TheGame.managers.logic.vars['time'] = 1;
+        TheGame.managers.logic.vars['day']++;
+        TheGame.managers.story.startScene('rest_short');
+        TheGame.resolveAction();
+        mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
+        mapBody.innerHTML='';
+        mapBody.style.display = "none";
+        TheGame.canvas.style.display = "block";
+        break;
     }
 
-    if (transition) {
-      btn.alpha = 0;
-      this.game.screenEffects.transition.FADEIN(btn);
-    } else {
-      room.pointAndClick.group.ignoreChildInput = true;
-    }
-  }
+    document.getElementById('morning_walk').addEventListener('click', () => {
+      TheGame.managers.story.startScene('morning_walk_' + Math.floor(Math.random() * (5 - 1) + 1));
+      TheGame.resolveAction();
+      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
+      mapBody.innerHTML='';
+      mapBody.style.display = "none";
+      TheGame.canvas.style.display = "block";
+    });
 
-  onCall(params) {
-    const action = params.body.split(" ")[0];
-    const obj = params.body.split(" ")[1];
-    const infoPaC = this.game.managers.logic.vars.pointAndClick;
-    const room = this.game.managers.background.current;
-
-    if (params.body === 'activate') {
-      room.pointAndClick.group.ignoreChildInput = false;
-      this.game.resolveAction();
-      return;
-    }
-
-    if (action === "remove") {
-      if (obj in room.pointAndClick.map) {
-        this.game.screenEffects.transition.FADEOUT(room.pointAndClick.map[obj]).then(() => {
-          if (!infoPaC.removed[room.name]) {
-            infoPaC.removed[room.name] = {};
-          }
-          infoPaC.removed[room.name][obj] = true;
-          if (infoPaC.added[room.name] && infoPaC.added[room.name][obj]) {
-            delete infoPaC.added[room.name][obj];
-          }
-          room.pointAndClick.map[obj].destroy();
-          this.game.resolveAction();
-        });
-        return;
+    document.getElementById('day_study').addEventListener('click', () => {
+      switch(TheGame.managers.logic.vars['mc_type']){
+        case 'technical':
+          TheGame.managers.story.startScene('OneHundredTwentyEight');
+          break;
+        case 'humanitarian':
+          TheGame.managers.story.startScene('AssembleWords');
+          break;
+        case 'designer':
+          TheGame.managers.story.startScene('SpotTheDifference');
+          break;
+        case 'sportsman':
+          TheGame.managers.story.startScene('SportClicker');
+          break;
+        case 'chemist':
+          TheGame.managers.story.startScene('Chemist');
+          break;
       }
-    }
+      TheGame.resolveAction();
+      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
+      mapBody.innerHTML='';
+      mapBody.style.display = "none";
+      TheGame.canvas.style.display = "block";
+    });
 
-    if (action === "add") {
-      if (!infoPaC.added[room.name]) {
-        infoPaC.added[room.name] = {};
-      }
-      infoPaC.added[room.name][obj] = {
-        name: params.name,
-        key: params.key ? params.key : params.name,
-        x: params.x - room.width / 2,
-        y: params.y - room.height / 2,
-        scene: params.scene
-      }
-
-      if (obj === 'park' && this.game.managers.logic.vars.playerPoints >= 10) {
-        this.createObject(room, { ...infoPaC.added[room.name][obj], key: 'park' }, "FADE");
-      } else {
-        this.createObject(room, { ...infoPaC.added[room.name][obj], key: 'home' }, "FADE");
-      }
-
-      if (infoPaC.removed[room.name] && infoPaC.removed[room.name][obj]) {
-        delete infoPaC.removed[room.name][obj];
-      }
-      this.game.resolveAction();
-      return;
-    }
-
-    this.game.resolveAction();
+    document.getElementById('day_friendly').addEventListener('click', () => {
+      TheGame.managers.story.startScene('morning_walk_' + Math.floor(Math.random() * (5 - 1) + 1));
+      TheGame.resolveAction();
+      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
+      mapBody.innerHTML='';
+      mapBody.style.display = "none";
+      TheGame.canvas.style.display = "block";
+    });
   }
 }
 
-RenJSGame.addPlugin('PointAndClick', PointAndClick);
+RenJSGame.addPlugin('GameMap', GameMap);
 
 
