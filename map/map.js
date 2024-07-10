f0d97d9e55af614c82cffd873c571253a231a35f
Lorenhow <105514629+Lorenhow@users.noreply.github.com>
2024-07-10 12:32:15 +0300

    0.6
    Some more scenes, morning map scripts finished. Day scripts partially done.

--- map/map.js
+++ map/map.js
@@ -1,19 +1,14 @@
 class GameMap extends RenJS.Plugin {
-  config = null;
-  infoPaC = null;
-
   onCall() {
     const TheGame = this.game;
     TheGame.canvas.style.display = "none";
     const mapBody = TheGame.canvas.parentElement.getElementsByTagName("div")[5];
     mapBody.style.display = "block";
 
-    mapBody.parentElement.setAttribute('style', 'background-image: url(http://127.0.0.1:5500/map/images/map.jpg); background-repeat: no-repeat; background-position: center');
 
     if (TheGame.managers.logic.vars['day'] == 5){
       TheGame.managers.story.startScene('exam_'+TheGame.managers.logic.vars['mc_type']);
       TheGame.resolveAction();
-      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
       mapBody.innerHTML='';
       mapBody.style.display = "none";
       TheGame.canvas.style.display = "block";
@@ -26,6 +21,38 @@ class GameMap extends RenJS.Plugin {
         morningButton.classList.add('walk_button');
         morningButton.id = 'morning_walk';
         mapBody.appendChild(morningButton);
+
+        let morning_type = Math.floor(Math.random() * 4)+1;
+
+        switch(morning_type){
+          case 1:
+            document.getElementById('morning_walk').style.left = '380px';
+            document.getElementById('morning_walk').style.top = '560px';
+            break;
+          case 2:
+            document.getElementById('morning_walk').style.left = '700px';
+            document.getElementById('morning_walk').style.top = '180px';
+            break;
+          case 3:
+            document.getElementById('morning_walk').style.left = '280px';
+            document.getElementById('morning_walk').style.top = '620px';
+            break;
+          case 4:
+            document.getElementById('morning_walk').style.left = '840px';
+            document.getElementById('morning_walk').style.top = '120px';
+            break;
+          case 5:
+            document.getElementById('morning_walk').style.left = '450px';
+            document.getElementById('morning_walk').style.top = '620px';
+            break;
+        }
+        document.getElementById('morning_walk').addEventListener('click', () => {
+          mapBody.innerHTML='';
+          mapBody.style.display = "none";
+          TheGame.canvas.style.display = "block";
+          TheGame.managers.story.startScene('morning_walk_' + morning_type);
+          TheGame.resolveAction();
+        });
         break;
       case 2:
         let dayButtonStudy = document.createElement('button');
@@ -36,6 +63,38 @@ class GameMap extends RenJS.Plugin {
         dayButtonFriendly.classList.add('frienly_button');
         dayButtonFriendly.id = 'day_friendly';
         mapBody.appendChild(dayButtonFriendly);
+
+        document.getElementById('day_study').addEventListener('click', () => {
+          mapBody.innerHTML='';
+          mapBody.style.display = "none";
+          TheGame.canvas.style.display = "block";
+          switch(TheGame.managers.logic.vars['mc_type']){
+            case 'technical':
+              TheGame.managers.story.startScene('OneHundredTwentyEight');
+              break;
+            case 'humanitarian':
+              TheGame.managers.story.startScene('AssembleWords');
+              break;
+            case 'designer':
+              TheGame.managers.story.startScene('SpotTheDifference');
+              break;
+            case 'sportsman':
+              TheGame.managers.story.startScene('SportClicker');
+              break;
+            case 'chemist':
+              TheGame.managers.story.startScene('Chemist');
+              break;
+          }
+          TheGame.resolveAction();
+        });
+
+        document.getElementById('day_friendly').addEventListener('click', () => {
+          mapBody.innerHTML='';
+          mapBody.style.display = "none";
+          TheGame.canvas.style.display = "block";
+          TheGame.managers.story.startScene('friend_meeting_2');
+          TheGame.resolveAction();
+        });
         break;
       case 3:
         let eveningButtonWalk = document.createElement('button');
@@ -55,57 +114,13 @@ class GameMap extends RenJS.Plugin {
         console.log(TheGame.managers.logic.vars['time']);
         TheGame.managers.logic.vars['time'] = 1;
         TheGame.managers.logic.vars['day']++;
-        TheGame.managers.story.startScene('rest_short');
-        TheGame.resolveAction();
-        mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
         mapBody.innerHTML='';
         mapBody.style.display = "none";
         TheGame.canvas.style.display = "block";
+        TheGame.managers.story.startScene('rest_short');
+        TheGame.resolveAction();
         break;
     }
-
-    document.getElementById('morning_walk').addEventListener('click', () => {
-      TheGame.managers.story.startScene('morning_walk_' + Math.floor(Math.random() * (5 - 1) + 1));
-      TheGame.resolveAction();
-      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
-      mapBody.innerHTML='';
-      mapBody.style.display = "none";
-      TheGame.canvas.style.display = "block";
-    });
-
-    document.getElementById('day_study').addEventListener('click', () => {
-      switch(TheGame.managers.logic.vars['mc_type']){
-        case 'technical':
-          TheGame.managers.story.startScene('OneHundredTwentyEight');
-          break;
-        case 'humanitarian':
-          TheGame.managers.story.startScene('AssembleWords');
-          break;
-        case 'designer':
-          TheGame.managers.story.startScene('SpotTheDifference');
-          break;
-        case 'sportsman':
-          TheGame.managers.story.startScene('SportClicker');
-          break;
-        case 'chemist':
-          TheGame.managers.story.startScene('Chemist');
-          break;
-      }
-      TheGame.resolveAction();
-      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
-      mapBody.innerHTML='';
-      mapBody.style.display = "none";
-      TheGame.canvas.style.display = "block";
-    });
-
-    document.getElementById('day_friendly').addEventListener('click', () => {
-      TheGame.managers.story.startScene('morning_walk_' + Math.floor(Math.random() * (5 - 1) + 1));
-      TheGame.resolveAction();
-      mapBody.parentElement.setAttribute('style', 'margin-top: 0px; margin-bottom: 0px; margin-left: 0px;margin-right: 0px;');
-      mapBody.innerHTML='';
-      mapBody.style.display = "none";
-      TheGame.canvas.style.display = "block";
-    });
   }
 }
 
